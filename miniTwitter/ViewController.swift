fd9e1c45fbe984d93336273b2b9b8630b9f6d391
kulraj singh <kulrajsingh@kulrajs-MacBook-Air.local>
2023-01-18 15:05:40 +0530

    project created

---
+++ miniTwitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miniTwitter
+//
+//  Created by kulraj singh on 18/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
